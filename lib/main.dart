ec5dc8edbe914b5e811eb66f1849a2fc529c55dd
SonyaLegis <sonya.legis@gmail.com>
2022-07-27 22:52:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// @dart=2.9
+import 'package:sonya_flutter_application/Dosen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Data Dosen',
+      home: Dosen(),
+    );
+  }
+}
